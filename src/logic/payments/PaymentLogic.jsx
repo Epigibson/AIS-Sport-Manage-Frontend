eeb15e595b7a0428e2528145116fef6b483c5f1c
Epigibson <hackminor@live.com.mx>
2024-03-01 15:26:41 -0600

    Initial: Se agrega la tabla de pagos.

--- src/logic/payments/PaymentLogic.jsx
+++ src/logic/payments/PaymentLogic.jsx
@@ -3,6 +3,7 @@ import { getAllHistoryPayments } from "../../api/PaymentService.jsx";
 import { PaymentColumns } from "./PaymentColumns.jsx";
 import { LoaderIconUtils } from "../../utils/LoaderIconUtils.jsx";
 import { TablesComponent } from "../../components/TablesComponent.jsx";
+import { getAllUsers } from "../../api/UserService.jsx";
 
 export const PaymentLogic = () => {
   const {
@@ -14,18 +15,25 @@ export const PaymentLogic = () => {
     queryFn: getAllHistoryPayments,
   });
 
-  // const { data: groupsData } = useQuery({
-  //     queryKey: ["allGroups"],
-  //     queryFn: getAllGroups,
-  // });
-  //
-  // const enrichedUsersData = usersData?.map((user) => {
-  //     const group = groupsData?.find((group) => group._id === user.group_id); // Ajusta según la estructura de tus datos
-  //     return { ...user, group }; // Añade la información del grupo al objeto de usuario
-  // });
+  const { data: usersData } = useQuery({
+    queryKey: ["allUsers"],
+    queryFn: getAllUsers,
+  });
+
+  const enrichedHistoryPaymentsData = historyPaymentData?.map(
+    (historyPayment) => {
+      const user = usersData?.find((user) => user._id === historyPayment.user); // Ajusta según la estructura de tus datos
+      return { ...historyPayment, user }; // Añade la información del grupo al objeto de usuario
+    },
+  );
 
   if (isLoading) return <LoaderIconUtils />;
   if (isError) return <h1>Error...</h1>;
 
-  return <TablesComponent data={historyPaymentData} columns={PaymentColumns} />;
+  return (
+    <TablesComponent
+      data={enrichedHistoryPaymentsData}
+      columns={PaymentColumns}
+    />
+  );
 };
