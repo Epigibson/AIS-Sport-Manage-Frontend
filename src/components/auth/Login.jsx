e765351c570005823d1aa17d7dc5008a62dbd2d6
Epigibson <hackminor@live.com.mx>
2024-04-01 14:40:01 -0600

    Feature: Se ajusta el logotipo en el login y el color de fondo.

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -20,13 +20,13 @@ export const Login = () => {
   }, []);
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-[#001529] dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
           className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-          <img className="w-10 h-10 mr-2" src={logo} alt="logo" />
+          <img className="w-28 h-28 mr-2" src={logo} alt="logo" />
         </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
