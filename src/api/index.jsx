fa2011d0efee05f370ed64fbe53eb571263fb171
Epigibson <hackminor@live.com.mx>
2024-04-12 00:40:45 -0600

    Feature: Se ajusta el dashboard principal.

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -3,9 +3,9 @@ import { getToken } from "../utils/tokenUtils.jsx";
 
 export const apiClient = axios.create({
   // baseURL: "http://127.0.0.1:8000/api/v1",
-  // baseURL: "https://be-dev-ziwi.onrender.com/api/v1",
+  baseURL: "https://be-dev-ziwi.onrender.com/api/v1",
   // baseURL: "https://ais-sport-manage.onrender.com/api/v1",
-  baseURL: "https://be-c031.onrender.com/api/v1",
+  // baseURL: "https://be-c031.onrender.com/api/v1",
 });
 
 apiClient.interceptors.request.use(
